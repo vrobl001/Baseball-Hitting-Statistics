64698807c16f8bd0fdc917ddee4b5b211dca6ac0
Vincent Robles <sylkisgsx@gmail.com>
2019-12-04 02:17:00 -0500

    select stats buttons now dynamically update info

--- js/script.js
+++ js/script.js
@@ -8,24 +8,30 @@ let rbiCheck;
 let obpCheck;
 let soCheck;
 
+
 document.getElementById('check-avg').addEventListener("click", (checkAvg) => {
   avgCheck = $("input[name='check-avg']:checked").val();
+  $('form').submit();
 })
 
 document.getElementById('check-hr').addEventListener("click", (checkhr) => {
   hrCheck = $("input[name='check-hr']:checked").val();
+  $('form').submit();
 })
 
 document.getElementById('check-rbi').addEventListener("click", (checkrbi) => {
   rbiCheck = $("input[name='check-rbi']:checked").val();
+  $('form').submit();
 })
 
 document.getElementById('check-obp').addEventListener("click", (checkobp) => {
   obpCheck = $("input[name='check-obp']:checked").val();
+  $('form').submit();
 })
 
 document.getElementById('check-so').addEventListener("click", (checkso) => {
   soCheck = $("input[name='check-so']:checked").val();
+  $('form').submit();
 })
 
 $(document).ready(function(){
@@ -51,7 +57,7 @@ $('form').on('submit', (event) => {
       let activeCheck = playerInfo.search_player_all.queryResults.row.active_sw;
       if(activeCheck === "Y") {document.getElementById('active-button').checked=true}
       else {document.getElementById('inactive-button').checked=true}
-      
+
       let league = playerInfo.search_player_all.queryResults.row.league;
       if(league === 'AL') {
         playerLeague = 'American League'
@@ -76,27 +82,48 @@ $('form').on('submit', (event) => {
 
   ).then(
     (playerStats) => {
-      console.log(playerStats)
+      console.log(playerStats);
+
       if(avgCheck === "on"){
       $('#avg').html("Hitting Average")
       $('#hitting-average').html(playerStats.sport_career_hitting.queryResults.row.avg);
+      } else {
+        $('#avg').html('')
+      $('#hitting-average').html('');
       };
+
       if(hrCheck === "on") {
       $('#hr').html("Homeruns")
       $('#hitting-homeruns').html(playerStats.sport_career_hitting.queryResults.row.hr);
+      } else {
+        $('#hr').html('')
+      $('#hitting-homeruns').html('');
       };
+
       if(rbiCheck === "on") {
       $('#rbi').html("Runs Batted In")
       $('#hitting-rbi').html(playerStats.sport_career_hitting.queryResults.row.rbi);
+      } else {
+        $('#rbi').html('')
+      $('#hitting-rbi').html('');
       };
+
       if(obpCheck === "on") {
       $('#obp').html("On Base Percentage")
       $('#hitting-obp').html(playerStats.sport_career_hitting.queryResults.row.obp);
+      } else {
+        $('#obp').html('')
+      $('#hitting-obp').html('');
       };
+
       if(soCheck === "on") {
       $('#so').html("Strike Outs")
       $('#hitting-so').html(playerStats.sport_career_hitting.queryResults.row.so);
+      } else {
+        $('#so').html('')
+      $('#hitting-so').html('');
       };
+
     }
   );
 })
